458e0572a1996a75bc57f7a5f556b4c6b025b684
Matt Christensen <mr.christensen@gmail.com>
2020-02-22 12:46:30 -0700

    Basic information

---
+++ README.md
@@ -0,0 +1,1 @@
+Uses the MealDB api - https://www.themealdb.com/api.php
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = function() {
-	setUp();
+    setUp();
 }
 
 function setUp() {
-	// FIXME - get ID
-	document.getElementById("").addEventListener("click", function(event) {
-		event.preventDefault();
+    // FIXME - get ID
+    document.getElementById("").addEventListener("click", function(event) {
+        event.preventDefault();
 
-		// FIXME - change is so that we get a desert and/or manin course
-		const url = "https://www.themealdb.com/api/json/v1/1/random.php";
-		fetch(url) 
-			.then(function(response) {
-				return response.json();
-			}).then(function(json) {
-				console.log(json);
-			});
-	});
+        // FIXME - change is so that we get a desert and/or manin course
+        const url = "https://www.themealdb.com/api/json/v1/1/random.php";
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                console.log(json);
+            });
+    });
 }
