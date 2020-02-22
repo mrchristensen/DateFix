3c1869e5396f7769142a8a055908f9fbc49d117b
cheyenneson <cheyenne.son@gmail.com>
2020-02-22 18:57:59 +0000

    added script

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	setUp();
+}
+
+function setUp() {
+	// FIXME - get ID
+	document.getElementById("").addEventListener("click", function(event) {
+		event.preventDefault();
+
+		// FIXME - change is so that we get a desert and/or manin course
+		const url = "https://www.themealdb.com/api/json/v1/1/random.php";
+		fetch(url) 
+			.then(function(response) {
+				return response.json();
+			}).then(function(json) {
+				console.log(json);
+			});
+	});
+}
