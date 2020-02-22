24a46d9c882f8843ae3b1127af9e72214f31cf97
cheyenneson <cheyenne.son@gmail.com>
2020-02-22 19:49:27 +0000

    fixed script

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 window.onload = function() {
-    setUp();
+	setUp();
 }
 
 function setUp() {
-<<<<<<< HEAD
-    // FIXME - get ID
-    document.getElementById("").addEventListener("click", function(event) {
-        event.preventDefault();
-
-        // FIXME - change is so that we get a desert and/or manin course
-        const url = "https://www.themealdb.com/api/json/v1/1/random.php";
-        fetch(url)
-            .then(function(response) {
-                return response.json();
-            }).then(function(json) {
-                console.log(json);
-            });
-    });
-}
-=======
 	// FIXME - get ID
 	document.getElementById("get-meal-button").addEventListener("click", function(event) {
 		event.preventDefault();
@@ -34,8 +18,6 @@ function setUp() {
 				results += "<h2>" + json.meals[0].strMeal + "</h2>";
 				results += "<h4>" + json.meals[0].strCategory + " 	" 
 						          + json.meals[0].strArea + "</h4>";
-				// let thumbnail = ;
-				// thumbnail = thumbnail.substring(0, thumbnail.length - 1);
 				results += "<img style=\"height: 256px;\" src=\"" 
 						+ json.meals[0].strMealThumb + "\"/>"; 
 
@@ -46,4 +28,3 @@ function setUp() {
 
 
 // meal and dessert
->>>>>>> 6a5089cace1184fcc8c49f65877d454b27000b70
